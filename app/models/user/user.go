76f612e8d0b21ddd17056dfd5c61018844389ac0
fivenull <fivenull@163.com>
2022-09-21 15:52:20 +0800

    用户密码加密

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -3,6 +3,7 @@ package user
 import (
 	"goblog/app/models"
 	"goblog/pkg/model"
+	"goblog/pkg/password"
 	"goblog/pkg/types"
 )
 
@@ -37,6 +38,6 @@ func GetByEmail(email string) (User, error) {
 }
 
 // ComparePassword 对比密码是否匹配
-func (user *User) ComparePassword(password string) bool {
-	return user.Password == password
+func (user *User) ComparePassword(_password string) bool {
+	return password.CheckHash(_password, user.Password)
 }
