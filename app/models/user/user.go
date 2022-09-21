0b5055dbf595459a6fe6d6b18a4446555e509876
fivenull <fivenull@163.com>
2022-09-21 15:29:02 +0800

    会话和认证管理

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"goblog/app/models"
+	"goblog/pkg/model"
+	"goblog/pkg/types"
 )
 
 // User 用户模型
@@ -13,4 +15,28 @@ type User struct {
 	Password string `gorm:"column:password;type:varchar(255)" valid:"password"`
 	// gorm:"-" —— 设置 GORM 在读写时略过此字段
 	PasswordConfirm string ` gorm:"-" valid:"password_confirm"`
+}
+
+// Get 通过 ID 获取用户
+func Get(idstr string) (User, error) {
+	var user User
+	id := types.StringToUint64(idstr)
+	if err := model.DB.First(&user, id).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// GetByEmail 通过 Email 来获取用户
+func GetByEmail(email string) (User, error) {
+	var user User
+	if err := model.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// ComparePassword 对比密码是否匹配
+func (user *User) ComparePassword(password string) bool {
+	return user.Password == password
 }
