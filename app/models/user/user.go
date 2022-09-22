ae44bd5837f9e74d35deaad5ee398fe9c0bd72a5
fivenull <fivenull@163.com>
2022-09-22 11:15:45 +0800

    用户的文章

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -4,6 +4,7 @@ import (
 	"goblog/app/models"
 	"goblog/pkg/model"
 	"goblog/pkg/password"
+	"goblog/pkg/route"
 	"goblog/pkg/types"
 )
 
@@ -43,6 +44,6 @@ func (user *User) ComparePassword(_password string) bool {
 }
 
 // Link 方法用来生成用户链接
-func (user *User) Link() string {
-	return ""
+func (user User) Link() string {
+	return route.Name2URL("users.show", "id", user.GetStringID())
 }
